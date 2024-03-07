c34bcd8fa856f5f49333c93bbcc7c576b7249b47
ahmadsarfraz18 <160363481+ahmadsarfraz18@users.noreply.github.com>
2024-03-07 11:57:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,159 @@
+// Arithmetic Operators
+
+// let Ali: number = 32
+// let saif: number = 23
+// let BothinTotal: Number = (Ali + saif);
+// console.log(BothinTotal);
+
+// let Ahmad: number = 55
+// let Imad: number = 32
+// let Total: number = (Ahmad + Imad);
+// console.log(Total);
+
+// let babar: number = 110
+// let nabeel: number = 333
+// let bothIntotal: number = (babar + nabeel);
+// console.log(bothIntotal);
+
+// let abrar: number = 90
+// let aslam: number = 55
+// let difference: number = (abrar - aslam);
+// console.log(difference)
+
+// let nomi: number = 88
+// let asad: number = 43
+// let difference: number = (nomi - asad);
+// console.log(difference);
+
+// let saad: number = 1000
+// let mani: number = 500
+// let difference: number = (saad - mani);
+// console.log(difference);
+
+// let ali: number = 15
+// let asif: number = 6
+// let mul: number = (ali * asif);
+// console.log(mul);
+
+// let saad: number = 90
+// let maani: number = 55
+// let mul: number = (saad * maani);
+// console.log(mul);
+
+// let akbar: number = 25
+// let asghar: number = 4
+// let mul: number = (akbar * asghar);
+// console.log(mul);
+
+// let ahmad: number = 55
+// let mohammad: number = 11
+// let div: number = (ahmad / mohammad);
+// console.log(div);
+
+// let bilal: number = 99
+// let sharif: number = 3
+// let div: number = (bilal / sharif);
+// console.log(div);
+
+// let adnan: number = 125
+// let salman: number = 5
+// let div: number = (adnan / salman);
+// console.log(div);
+
+// let asd: number = 46
+// let jkl: number = 8
+// let rem: number = (asd % jkl);
+// console.log(rem);
+
+// let ali: number = 89
+// let asad: number = 10
+// let remainder: number = (ali % asad);
+// console.log(remainder)
+
+// let imran: number = 20
+// let umar: number = 2
+// let power: number = (imran ** umar);
+// console.log(power);
+
+// let faisal: number = 90
+// let sajid:  number = 3
+// let power: number = (faisal ** sajid);
+// console.log(power);
+
+// let firstName: string = "Ahmad";
+// let lastName: string = "Sarfraz";
+// console.log(firstName + lastName);
+
+// let firstName: string = "Mohammad";
+// let lastName: string = "Ahmad Mujtaba";
+// console.log(firstName + lastName)
+
+// let firstName: string = "Ahmad";
+// let lastName: string = "Sarfraz";
+// console.log(firstName + " " + lastName);
+
+
+// let firstName: string = "Mohammad";
+//  let lastName: string = "Ahmad Mujtaba";
+//  console.log(firstName + " "  + lastName)
+
+// let hadi: number = 50
+// hadi = hadi + 100
+// console.log(hadi)
+
+// let  baabu: number = 1000
+// baabu = baabu + 100
+// console.log(baabu)
+
+// Comparison Operators;
+
+// let myself: number = 33
+// let yourself: number = 22
+// console.log(myself == yourself);
+
+// let myself: number = 33
+// let yourself: number = 33
+// console.log(myself == yourself);           ==
+
+// let myself: number = 33
+// let yourself: number = 33
+// console.log(myself === yourself);          ===
+
+// let myself: number = 33
+// let yourself: number = 50
+// console.log(myself !=  yourself);          !=
+
+//  let myself: number = 33
+//  let yourself: number = 50
+// console.log(myself > yourself);             >
+
+//  let myself: number = 33
+//  let yourself: number = 50
+// console.log(myself < yourself);             <  
+
+// let myself: number = 33
+// let yourself: number = 50
+// console.log(myself <= yourself);            <=
+
+// let myself: number = 33
+// let yourself: number = 50
+// console.log(myself >= yourself);            >=
+
+// console.log(10 == "10");
+
+// let a = 35+15
+// a = a + 25
+// console.log(a);
+
+// let A = 75+75
+// A = A + 50
+// console.log(A);
+
+// let d = 55-35
+// d = d - 5
+// console.log(d);
+
+
+
+
+
